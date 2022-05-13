4e2c85647c12e9f025d8cadcfbaafa2f8529e480
Jonathan Conrad <jc@Jonathans-MBP.home>
2022-05-13 10:42:12 -0700

    Leveled the playing odds for wether the robot or the dinosaur wins

--- robot.py
+++ robot.py
@@ -3,8 +3,8 @@ from weapon import Weapon
 class Robot:
     def __init__(self, name):
         self.robot_name = name
-        self.robot_health = int(100)
-        self.robot_weapon = Weapon('Varatha, the Eternal Spear', int(36))
+        self.robot_health = int(120)
+        self.robot_weapon = Weapon('Varatha, the Eternal Spear', int(38))
         self.robot_arsenal = []
 
     def attack(self, dinosaur):
@@ -12,10 +12,10 @@ class Robot:
         return health_lvl
 
     def arsenal(self):
-        self.robot_arsenal.append(Weapon('Varatha, the Eternal Spear', int(36)))
-        self.robot_arsenal.append(Weapon('Sword of the Stone', int(30)))
-        self.robot_arsenal.append(Weapon('Bloodstone Bludgeon', int(42)))
-        self.arsenal_select = input(f"Select your weapon:\nA) Varatha, the Eternal Spear (36 DPS)\nB) Sword of the Stone (32 DPS)\nC) Bloodstone Bludgeon (42 DPS)\nAnswer: ")
+        self.robot_arsenal.append(Weapon('Varatha, the Eternal Spear', int(38)))
+        self.robot_arsenal.append(Weapon('Sword of the Stone', int(32)))
+        self.robot_arsenal.append(Weapon('Bloodstone Bludgeon', int(45)))
+        self.arsenal_select = input(f"Select your weapon:\nA) Varatha, the Eternal Spear (38 DPS)\nB) Sword of the Stone (32 DPS)\nC) Bloodstone Bludgeon (45 DPS)\nAnswer: ")
         if self.arsenal_select == 'A':
             self.robot_weapon = self.robot_arsenal[0]
         elif self.arsenal_select == 'B':
